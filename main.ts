85705be70ff066737ddd035682b3db9b8cf8892d
ppilaszewicz <71039101+ppilaszewicz@users.noreply.github.com>
2021-03-04 11:44:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+//  
+input.onButtonPressed(Button.A, function () {
+    strikes += 1
+    basic.showNumber(strikes)
+})
+// 
+input.onButtonPressed(Button.B, function () {
+	
+})
+let strikes = 0
+let balls = 0
+strikes += 0
+basic.forever(function () {
+	
+})
